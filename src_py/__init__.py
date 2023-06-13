0209c7ae9a95bb6f0d746f8e1dbaabe5f0408b55
Mika <mika.oesterreich@icloud.com>
2023-06-13 21:22:28 +0200

    add seed function as key alternative

--- src_py/__init__.py
+++ src_py/__init__.py
@@ -1,15 +1,19 @@
 import numpy as np
+import random
 
 class chiffre:
-    def __init__(self, size, key) -> None:
+    def __init__(self, size, key="", seed=None) -> None:
         self.square = np.zeros((size, size), dtype=str)
         self.key = key
         self.size = size
 
         for i in range(len(self.key)):
             self.square[i // len(self.square)][i % len(self.square)] = self.key[i]
-        abc = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789.!?,:;()[]{}+-*/=<>|_@#$%&"
+        abc = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.!?,"
         abc = "".join([x for x in abc if x not in self.key])
+        if seed !=None:
+            random.seed(seed)
+            abc = "".join(random.sample(abc, len(abc)))
         if len(abc) > len(self.square) ** 2: print("Warning: not all letters of abc are used in square!")
         # for letter in abc: put on first free spot in square
         for letter in abc:
